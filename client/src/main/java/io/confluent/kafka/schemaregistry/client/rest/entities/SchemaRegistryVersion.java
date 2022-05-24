ea5cce221f2e170c13df1a1eb80b741581a815b5
Anshul-Goyal <agoyal@confluent.io>
2022-05-24 15:09:03 +0530

    DGS-3141 - fix checkstyle

--- client/src/main/java/io/confluent/kafka/schemaregistry/client/rest/entities/SchemaRegistryVersion.java
+++ client/src/main/java/io/confluent/kafka/schemaregistry/client/rest/entities/SchemaRegistryVersion.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package io.confluent.kafka.schemaregistry.client.rest.entities;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -11,51 +26,51 @@ import java.util.Objects;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SchemaRegistryVersion {
 
-	private String version;
-	private String commitId;
+  private String version;
+  private String commitId;
 
-	@JsonCreator
-	public SchemaRegistryVersion(@JsonProperty("version") String version,
-															 @JsonProperty("commitId") String commitId) {
-		this.version = version;
-		this.commitId = commitId;
-	}
+  @JsonCreator
+  public SchemaRegistryVersion(@JsonProperty("version") String version,
+                               @JsonProperty("commitId") String commitId) {
+    this.version = version;
+    this.commitId = commitId;
+  }
 
-	@JsonProperty("version")
-	public String getVersion() {
-		return version;
-	}
+  @JsonProperty("version")
+  public String getVersion() {
+    return version;
+  }
 
-	@JsonProperty("version")
-	public void setVersion(String version) {
-		this.version = version;
-	}
+  @JsonProperty("version")
+  public void setVersion(String version) {
+    this.version = version;
+  }
 
-	@JsonProperty("commitId")
-	public String getCommitId() {
-		return commitId;
-	}
+  @JsonProperty("commitId")
+  public String getCommitId() {
+    return commitId;
+  }
 
-	@JsonProperty("commitId")
-	public void setCommitId(String commitId) {
-		this.commitId = commitId;
-	}
+  @JsonProperty("commitId")
+  public void setCommitId(String commitId) {
+    this.commitId = commitId;
+  }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		SchemaRegistryVersion srVersion = (SchemaRegistryVersion) o;
-		return Objects.equals(version, srVersion.version)
-						&& Objects.equals(commitId, srVersion.commitId);
-	}
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    SchemaRegistryVersion srVersion = (SchemaRegistryVersion) o;
+    return Objects.equals(version, srVersion.version)
+            && Objects.equals(commitId, srVersion.commitId);
+  }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(version, commitId);
-	}
+  @Override
+  public int hashCode() {
+    return Objects.hash(version, commitId);
+  }
 }
